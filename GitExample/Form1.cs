1918866807f03ae3c7f7490a62346d37adbe2cf6
chispalex89 <chispalex89@gmail.com>
2022-01-20 08:18:02 -0600

    added checkbox

--- GitExample/Form1.cs
+++ GitExample/Form1.cs
@@ -16,5 +16,10 @@ namespace GitExample
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK");
+        }
     }
 }
